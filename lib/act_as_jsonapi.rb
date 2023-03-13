742e5bb6aed9ebfaa195e4965f3ed998b825d2a6
leizzer <lizzydw@gmail.com>
2023-03-13 10:40:28 -0300

    first commit

---
+++ lib/act_as_jsonapi.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "act_as_jsonapi/version"
+
+module ActAsJsonapi
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/act_as_jsonapi/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module ActAsJsonapi
+  VERSION = "0.1.0"
+end
